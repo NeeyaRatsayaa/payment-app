259337d2d4c16c09624f84e95784652547879ce7
NeeyaRatsayaa <neeya.ratsayaa@gmail.com>
2024-06-06 23:18:32 +0700

    re-build and re-route page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { PaymentFromPageComponent } from './payment-from-page/payment-from-page.component';
 
 export const routes: Routes = [
-    {path: 'payment-form', component: PaymentFromPageComponent},
+    {path: '', component: PaymentFromPageComponent},
 ];
